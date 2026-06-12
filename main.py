78239aead6a437ca84069f3797e0bc2d5c4f55aa
rodaraujo-creator <rod.araujo@duocuc.cl>
2026-06-12 17:52:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,81 @@
-acum=0
-descuento=0
-subtotal=0
-cantidad=0
-ciclo = True
-while ciclo:
-    print('''
-    Producto            Unidades Medida           Precio
-    -----------------------------------------------------
-    1) Lipigas          15 kilos                  $15.500
-    2) Gasco            11 kilos                  $10.000
-    3) Gasco            15 kilos                  $13.000
-    4) Abastible        45 kilos                  $45.000
-    ''')
+# Funciones: sin parámetros / sin retorno
+#           sin parámetros / con retorno
+#           con parámetros / sin retorno
+#           con parámetros / con retorno
+#Evaluación: Listas, colecciones y funciones
+#Proceso de ingresar, eliminar, listar, buscar y modificar.
+# Contexto: Ingreso de productos en una tienda, cada producto
+# posee: un codigo, nombre de producto, precio
+#validacion: - codigo: numero positivo mayor a cero
+#           - nombre: texto sin espacios y minimo tres caracteres.
+#           - Precio: numero positivo entre 100 y 50000
+lista_productos=[]
+def validaCodigo(cod):
+    try:
+        c = int(cod)
+        if c>0:
+            return True
+        return False
+    except:
+        return False
+def validarNombre(nom):
+    if len(nom.strip())>=3:
+        return True
+    return False
+def validaPrecio(precio):
+    try:
+        p=int(precio)
+        if p>=100 and p<=50000:
+            return True
+        return False
+    except:
+        return False  
+def ingresarProducto(lista):
+    codigo=input("Ingrese codigo del producto:")
+    resp = validaCodigo(hh="hola", cod=codigo)
+    if resp == False:
+        print("Ingrese codigo numerico entero mayor a cero")
+        return False
+    nombre=input("ingrese nombre del producto:")
+    resp = validarNombre(nombre)
+    if resp==False:
+        print("nombre sin espacios y minimo 3 letras")
+        precio=input("ingrese precio del producto:")
+        resp=validarNombre(nombre)
+        if resp==False:
+            print("nombre sin espacios y minimo 3 letras")
+        return False
+def menu():
+    print("== menu almacen ==")
+    print("1. Ingresar")
+    print("2. Listar")
+    print("3. Salir")
+
+def seleccione():
+    try:
+        op=int(input("Seleccione:"))
+        if op>=1 and op<=3:
+            return op
+        print("opciones entre 1 y 3")
+        return op
+    except:
+        print("opcion incorrecta, ingrese solo numeros")
+        return 0
     
-    op = int(input("Seleccione: "))
-    if op >=1 and op <=4 :
-        cantidad = int(input("Ingrese cantidad: "))
+    
+def rutas(op):
     match op:
         case 1:
-            subtotal = 15500 * cantidad
+            print("== Ingresar ==")
         case 2:
-            subtotal = 10000 * cantidad
+            print("== Listar ==")
         case 3:
-            subtotal = 13000 * cantidad
-        case 4:
-            subtotal * 45000 * cantidad
-        case 5:
-            ciclo = False
-        case 6:
-            acum = 0
-        case _:
-            print("Opción incorrecta")
-    print (f"Tu subtotal es: {subtotal}")
-    if op!=2 and cantidad>=3:
-        match op:
-                case 1:
-                    descuento = subtotal * 0.1
-                case 3:
-                    descuento = subtotal * 0.05
-                case 4:
-                    descuento = subtotal * 0.03
-    print(f"Total a pagar: {subtotal-descuento}")
-    acum = acum + (subtotal-descuento)
-    subtotal=0
-    descuento=0
-    print(f"Total a pagar: {acum}")
+            print("== Salir ==")
+ciclo = True
+while ciclo:
+    menu()
+    opcion = seleccione()
+    rutas(opcion)
+    if opcion==3:
+        ciclo=False
+
